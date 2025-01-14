5f8faf9923cf772ecc12e7e71d6ad4cc4ca95bad
nuruddin6153 <nuruddin6153@gmail.com>
2025-01-14 10:49:26 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
